016b29f05605188727a0c00f7c9a927a802ef8d9
stevensc9277 <stevensc9277@masseyhigh.school.nz>
2021-04-15 13:06:39 +1200

    working yes_no function and number checker

---
+++ 02_yes_no_check_v1.py
@@ -0,0 +1,20 @@
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    while True:
+        response =  input(question).lower()
+    
+        for var_item in to_check:
+            if response == var_item:
+                return response
+
+            elif response == var_item[0]:
+                return var_item 
+        
+        print("Please enter either yes or no...\n")
+
+# Loops to make testing faster 
+for item in range(0, 6):
+    want_help = yes_no("Do you want to read the instructions? ")
+    print("You said '{}'\n".format(want_help))
