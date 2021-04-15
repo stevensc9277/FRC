a2d34fd2f4c43e2e574c867c0d55f2f4623b1f18
stevensc9277 <stevensc9277@masseyhigh.school.nz>
2021-04-16 11:57:47 +1200

    Working code for variable cost calc. and data frames

--- 03_variable_costs_v1.py
+++ 03_variable_costs_v1.py
@@ -15,20 +15,73 @@ def num_check(question, error, type):
         except ValueError:
             print(error)
 
-def yes_no(question):
+def not_blank(question, error):
 
-    to_check = ["yes", "no"]
 
     while True:
-        response =  input(question).lower()
+        response =  input(question)
     
-        for var_item in to_check:
-            if response == var_item:
-                return response
+        if response == "":
+            print("{}. \nPlease try again.\n".format(error))
+            continue
 
-            elif response == var_item[0]:
-                return var_item 
-        
-        print("Please enter either yes or no...\n")
+        return response
 
 # currency formatting function
+def currency(x):
+    return "{:.2f}".format(x)
+
+# *** Main routine starts here ***
+
+# Set up dictionaries and lists
+
+item_list = []
+quantity_list = []
+price_list = []
+
+variable_dict = {
+    "Item": item_list,
+    "Quantity": quantity_list,
+    "Price": price_list
+}
+
+# Get user data
+product_name = not_blank("Product name: ", "The product name can't be blank.")
+
+# loop to get component, quantity and price
+item_name = ""
+while item_name.lower() != "xxx":
+
+    print()
+    # get name, quantity and item
+    item_name = not_blank("Item name: ", "The component name can't be blank.")
+
+    if item_name.lower() == "xxx":
+        break
+
+    quantity = num_check("Quantity: ", "The amount must be a whole number more than zero", int)
+    price = num_check("How much for a single item? $", "The price must be a number more than zero", float)
+
+    # add item, quantity and price to lists
+    item_list.append(item_name)
+    quantity_list.append(quantity)
+    price_list.append(price)
+
+variable_frame = pandas.DataFrame(variable_dict)
+variable_frame = variable_frame.set_index('Item')
+
+# Calculate cost of each component
+variable_frame['Cost'] = variable_frame['Quantity'] * variable_frame['Price']
+
+# Find sub total
+variable_sub = variable_frame['Cost'].sum()
+
+# Currency Formatting (uses currency function)
+add_dollars = ['Price', 'Cost']
+for item in add_dollars:
+    variable_frame[item] = variable_frame[item].apply(currency)
+
+# *** Printing Area ***
+print(variable_frame)
+print()
+print("Variable Costs: ${:.2f}".format(variable_sub))
