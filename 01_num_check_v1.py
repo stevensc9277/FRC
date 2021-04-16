1f0156bbb2d8fead006e74b0cec1c9d747bf10b6
stevensc9277 <stevensc9277@masseyhigh.school.nz>
2021-04-16 12:19:56 +1200

    to finish

---
+++ 01_num_check_v1.py
@@ -0,0 +1,22 @@
+def num_check(question, error, type):
+    
+    while True:
+        try:
+            response = type(input(question))
+
+            if response <= 0:
+                print(error)
+                print()
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# Main routine goes here
+get_int = num_check("How many do you need? ","Please enter an integer (whole number) that is more than 0\n", int)
+get_cost = num_check("How much does it cost? $","Please enter an integer (whole number) that is more than 0\n", float)
+
+print("You need: ", get_int)
+print("It costs: $", get_cost)
