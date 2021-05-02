1c04047f48bb8b5ca819e9f840b312c86de4f23e
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-05-03 10:43:29 +1200

    calculates costs for stuff

---
+++ 00_FC_Base_v2.py
@@ -0,0 +1,126 @@
+import pandas
+
+def num_check(question, error, type):
+    
+    while True:
+        try:
+            response = type(input(question))
+
+            if response <= 0:
+                print(error)
+                print()
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+def not_blank(question, error):
+
+
+    while True:
+        response =  input(question)
+    
+        if response == "":
+            print("{}. \nPlease try again.\n".format(error))
+            continue
+
+        return response
+
+# currency formatting function
+def currency(x):
+    return "{:.2f}".format(x)
+
+
+# Gets expenses, returns lists which has the data frame and sub total
+def get_expenses(var_fixed):
+    # set up dictionaries and lists
+
+    item_list = []
+    quantity_list = []
+    price_list = []
+
+    variable_dict = {
+        "Item": item_list,
+        "Quantity": quantity_list,
+        "Price": price_list
+    }
+
+    
+
+    # loop to get component, quantity and price
+    item_name = ""
+    while item_name.lower() != "xxx":
+
+        print()
+        # get name, quantity and item
+        item_name = not_blank("Item name: ", "The component name can't be blank.")
+
+        if item_name.lower() == "xxx":
+            break
+        
+        if var_fixed == "variable":
+            quantity = num_check("Quantity: ", "The amount must be a whole number more than zero", int)
+
+        else:
+            quantity = 1
+        
+        price = num_check("How much ? $", "The price must be a number more than zero", float)
+
+        # add item, quantity and price to lists
+        item_list.append(item_name)
+        quantity_list.append(quantity)
+        price_list.append(price)
+
+        
+    expense_frame = pandas.DataFrame(variable_dict)
+    expense_frame = expense_frame.set_index('Item')
+
+    # Calculate cost of each component
+    expense_frame['Cost'] = expense_frame['Quantity'] * expense_frame['Price']
+
+    # Find sub total
+    sub_total = expense_frame['Cost'].sum()
+
+    # Currency Formatting (uses currency function)
+    add_dollars = ['Price', 'Cost']
+    for item in add_dollars:
+        expense_frame[item] = expense_frame[item].apply(currency)
+
+    return [expense_frame, sub_total]
+# *** Main routine starts here ***
+
+# Get product name
+product_name = not_blank("Product name: ", "The product name")
+
+# Get variable costs
+variable_expenses = get_expenses("variable")
+variable_frame = variable_expenses[0]
+variable_sub = variable_expenses[1]
+
+# Get fixed costs
+fixed_expenses = get_expenses("fixed")
+fixed_frame = fixed_expenses[0]
+fixed_sub = fixed_expenses[1]
+
+# Find total costs
+
+# Ask user for profit goal
+
+# Calculate recommended price
+
+# Write data to file
+
+# Printing area
+
+print("*** Variable Costs ***")
+print(variable_frame)
+print()
+
+print("Variable Costs: ${:.2f}".format(variable_sub))
+
+print("*** Fixed Costs ***")
+print(fixed_frame[['Cost']])
+print()
+
+print("Fixed Costs: ${:.2f}".format(fixed_sub))
---
+++ 04_fixed_costs_v1.py
@@ -0,0 +1,107 @@
+import pandas
+
+def num_check(question, error, type):
+    
+    while True:
+        try:
+            response = type(input(question))
+
+            if response <= 0:
+                print(error)
+                print()
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+def not_blank(question, error):
+
+
+    while True:
+        response =  input(question)
+    
+        if response == "":
+            print("{}. \nPlease try again.\n".format(error))
+            continue
+
+        return response
+
+# currency formatting function
+def currency(x):
+    return "{:.2f}".format(x)
+
+
+# Gets expenses, returns lists which has the data frame and sub total
+def get_expenses(var_fixed):
+    # set up dictionaries and lists
+
+    item_list = []
+    quantity_list = []
+    price_list = []
+
+    variable_dict = {
+        "Item": item_list,
+        "Quantity": quantity_list,
+        "Price": price_list
+    }
+
+    # Get user data
+    product_name = not_blank("Product name: ", "The product name can't be blank.")
+
+    # loop to get component, quantity and price
+    item_name = ""
+    while item_name.lower() != "xxx":
+
+        print()
+        # get name, quantity and item
+        item_name = not_blank("Item name: ", "The component name can't be blank.")
+
+        if item_name.lower() == "xxx":
+            break
+        
+        if var_fixed == "variable":
+            quantity = num_check("Quantity: ", "The amount must be a whole number more than zero", int)
+
+        else:
+            quantity = 1
+        
+        price = num_check("How much ? $", "The price must be a number more than zero", float)
+
+        # add item, quantity and price to lists
+        item_list.append(item_name)
+        quantity_list.append(quantity)
+        price_list.append(price)
+
+        
+    expense_frame = pandas.DataFrame(variable_dict)
+    expense_frame = expense_frame.set_index('Item')
+
+    # Calculate cost of each component
+    expense_frame['Cost'] = expense_frame['Quantity'] * expense_frame['Price']
+
+    # Find sub total
+    sub_total = expense_frame['Cost'].sum()
+
+    # Currency Formatting (uses currency function)
+    add_dollars = ['Price', 'Cost']
+    for item in add_dollars:
+        expense_frame[item] = expense_frame[item].apply(currency)
+
+    return [expense_frame, sub_total]
+# *** Main routine starts here ***
+
+# Get product name
+# product_name = not_blank("Product name: ", "The product name")
+
+fixed_expenses = get_expenses("Fixed")
+fixed_frame = fixed_expenses[0]
+fixed_sub = fixed_expenses[1]
+
+# Printing area
+
+print()
+print(fixed_frame[['Cost']])
+print()
+
+print("Fixed Costs: ${:.2f}".format(fixed_sub))
