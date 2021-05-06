972d41caed4e18af4751781d11983a4b5f9e3e74
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2021-05-07 11:46:34 +1200

    profit goal calculator

--- 05_profit_goal.py
+++ 05_profit_goal.py
@@ -19,9 +19,70 @@ def yes_no(question):
 
 
 def profit_goal(total_costs):
-    increase = int(input("Profit goal? ", "%"))
-    target = total_costs * (increase/100)
-    return "${}".format(target)
+    # initialise variables and error message
+    error = "Please enter a valid profit goal \n"
+
+    while True:
+        # ask for profit goal
+        response = input("What is your profit goal (eg $500 or 50%)? ")
+
+        # check if first character is $
+        if response[0] == "$":
+            profit_type = "$"
+            # Get amount (everything after the $)
+            amount = response [1:]
+        
+        # check if last character is %
+        elif response [-1] == %:
+            profit_type = "%"
+            # Get amount (everything before %)
+            amount = response[:-1]
+        
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # Check amount is a number more than zero
+            amount = float(amount)
+            if amount <= 0:
+                print(error)
+                continue
+        
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unknown" and amount >= 100:
+            dollar_type = yes_no("Do you mean ${:.2f}. ie {:.2f} dollars? y/n? ".format(amount, amount))
+
+            # Set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            percent_type = yes_no("Do you mean {}% , y / n: ".format(amount))
+
+            if percent_type == "yes":
+                profit_type = "%"
+
+            else:
+                profit_type = "$"
+        
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
+
+
+
 
 # Loop for quick testing
 all_costs = 200
